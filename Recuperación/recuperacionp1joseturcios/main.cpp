ce1478bc24ee7a9f5aed1d2595f9b68cff3693c3
Jose Turcios <joseturcios1579@gmail.com>
2023-06-21 11:41:56 -0600

    Usuario y main

--- Recuperación/recuperacionp1joseturcios/main.cpp
+++ Recuperación/recuperacionp1joseturcios/main.cpp
@@ -1,9 +1,58 @@
 #include <iostream>
+#include <cstdlib>
+#include<fstream>
+#include<stdlib.h>
+#include<cstdlib>
+#include<conio.h>
+#include<iomanip>
+#include "Usuario.h"
 
+Usuario llamaPortada;
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    llamaPortada.dibujarPortada("Portada.txt");
+    Usuario IngresoUsuario;
+    int numero;
+    do
+    {
+    system("cls");
+    cout<<"\t\t\t--------------------------------------------------"<<endl;
+	cout<<"\t\t\t|        Bienvenid@s a Cines el Mero Oscar!      |"<<endl;
+	cout<<"\t\t\t--------------------------------------------------"<<endl;
+	cout<<"\n\t\t\t 1. Iniciar sesion "<<endl;
+	cout<<"\t\t\t 2. Registrar nuevo Usuario"<<endl;
+	cout<<"\t\t\t 3. Salir del Sistema"<<endl;
+
+    cout << "\n\t\t\t---------------------------------------";
+    cout << "\n\t\t\t| Opciones a escoger: [1] - [2] - [3]  |";
+    cout << "\n\t\t\t---------------------------------------";
+
+	cout<< " "<<endl;
+	cout<<"\n\n\t\t\tingrese la opcion a escoger: ";
+	cin>>numero;
+
+        switch(numero)
+        {
+            case 1:
+                IngresoUsuario.iniciarSesion();
+                break;
+
+            case 2:
+                IngresoUsuario.registroUsuario();
+                break;
+
+            case 3:
+                exit(0);
+                break;
+
+            default:
+                cout<<"\t\t\tIngrese una opcion valida"<<endl;
+                cin.get();
+        }
+    } while(numero!= 3);
+
+
     return 0;
 }
