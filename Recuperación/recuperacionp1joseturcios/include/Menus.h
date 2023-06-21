4b0be692cd410031dc3c26889ec780969315f23c
Jose Turcios <joseturcios1579@gmail.com>
2023-06-21 11:28:29 -0600

    Menus

---
+++ Recuperación/recuperacionp1joseturcios/include/Menus.h
@@ -0,0 +1,26 @@
+#ifndef MENUS_H
+#define MENUS_H
+#include <iostream>
+#include<fstream>
+#include<stdlib.h>
+#include<cstdlib>
+#include<conio.h>
+#include<iomanip>
+
+using namespace std;
+
+class Menus
+{
+    public:
+        void menuInicial(string nombreUsuario);
+        void menuCatalogos(string nombreUsuario);
+        void subMenuEmpleados(string nombreUsuario);
+        void subMenuInformes(string nombreUsuario);
+        void menuProcesos(string nombreUsuario);
+
+    protected:
+
+    private:
+};
+
+#endif // MENUS_H
